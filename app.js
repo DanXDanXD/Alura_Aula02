86d4e723c56a4b0af488ed4e830b400b85d187fd
Adauto Dan <123683397+DanXDanXD@users.noreply.github.com>
2025-03-11 12:43:18 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1')
+tituto.innerHMTL = 'jogo do sem numero';
+
+let paragrafo = document.querySelector('p')
+paragrafo.innerHTML = 'Escolhe o numero sem numero de 1 a 10';
+
+function verificarChute() {
+    console.log ('O botao foi clicado ;)')
+}
